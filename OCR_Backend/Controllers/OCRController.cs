88c5b99260db26cdcfe56ba9b020fcf8387f12ab
Kyle Lamoureux <klamoureux386@gmail.com>
2022-09-04 15:10:32 -0400

    Sample setup

---
+++ OCR_Backend/Controllers/OCRController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OCR_Backend.Services;
+
+namespace OCR_Backend.Controllers
+{
+    public class OCRController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetOCR()
+        {
+            //To do: convert to DI
+            return Ok(new OCRService().OCRServiceTest());
+        }
+    }
+}
---
+++ OCR_Backend/Services/OCRService.cs
@@ -0,0 +1,10 @@
+﻿namespace OCR_Backend.Services
+{
+    public class OCRService
+    {
+        public string OCRServiceTest() 
+        {
+            return "This is a test string sent from the service.";
+        }
+    }
+}
